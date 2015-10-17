3f3f7048124627acd97e030daa286ecaee717bd6
prataprc <prataprc@gmail.com>
2015-10-17 13:22:57 +0530

    udpated to client.sh script

--- tools/requests/client.sh
+++ tools/requests/client.sh
@@ -10,10 +10,10 @@ time ./requests -addr $1 -conns 16 -routines 100 -count 10000
 echo
 
 echo "building client profile information ..."
-go tool pprof -svg requests requests.pprof > server.pprof.svg
-go tool pprof -inuse_space -svg requests requests.mprof > server.ispace.svg
-go tool pprof -inuse_objects -svg requests requests.mprof > server.iobjs.svg
-go tool pprof -alloc_space -svg requests requests.mprof > server.aspace.svg
-go tool pprof -alloc_objects -svg requests requests.mprof > server.aobjs.svg
+go tool pprof -svg requests requests.pprof > requests.pprof.svg
+go tool pprof -inuse_space -svg requests requests.mprof > requests.ispace.svg
+go tool pprof -inuse_objects -svg requests requests.mprof > requests.iobjs.svg
+go tool pprof -alloc_space -svg requests requests.mprof > requests.aspace.svg
+go tool pprof -alloc_objects -svg requests requests.mprof > requests.aobjs.svg
 echo
 
