17ead78a6f4ccd48216c005c132b735a2cced2cc
prataprc <prataprc@gmail.com>
2015-10-31 22:11:36 +0530

    bugfix: tag_gzip.

--- tag_gzip.go
+++ tag_gzip.go
@@ -6,26 +6,23 @@ import "compress/gzip"
 import "bytes"
 
 func make_gzip(t *Transport, config map[string]interface{}) (uint64, tagfn, tagfn) {
-	buffersize := config["buffersize"].(int)
 	level := config["gzip.level"].(int)
-	wbuf := bytes.NewBuffer(make([]byte, 0, buffersize))
-	writer, err := gzip.NewWriterLevel(wbuf, level)
-	if err != nil {
-		panic(err)
-	}
 	enc := func(in, out []byte) int {
 		if len(in) == 0 { // empty input
 			return 0
 		}
-		wbuf.Reset()
-		writer.Reset(wbuf)
-		_, err := writer.Write(in)
+		wbuf := bytes.NewBuffer(out[:0])
+		writer, err := gzip.NewWriterLevel(wbuf, level)
+		if err != nil {
+			panic(err)
+		}
+		_, err = writer.Write(in)
 		if err != nil {
 			panic(err)
 		} else if err = writer.Flush(); err != nil {
 			panic(err)
 		}
-		return copy(out, wbuf.Bytes())
+		return wbuf.Len()
 	}
 	dec := func(in, out []byte) int {
 		if len(in) == 0 {
@@ -45,5 +42,5 @@ func make_gzip(t *Transport, config map[string]interface{}) (uint64, tagfn, tagf
 }
 
 func init() {
-	tag_factory["gzip"] = make_lzw
+	tag_factory["gzip"] = make_gzip
 }
