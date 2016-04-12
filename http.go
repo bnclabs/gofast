44015279c8a54d11226578621f89bbcf0e7cd5eb
prataprc <prataprc@gmail.com>
2016-04-12 11:18:11 +0530

    http: default http handlers for /gofast/statistics endpoint.

---
+++ http.go
@@ -0,0 +1,54 @@
+package gofast
+
+import "strings"
+import "encoding/json"
+import "net/http"
+import "time"
+
+// Statshandler http handler to handle statistics endpoint, returns
+// statistics for specified transport or aggregate statistics of all
+// transports, based on the query parameters.
+func Statshandler(w http.ResponseWriter, r *http.Request) {
+	if query := r.URL.Query(); query != nil {
+		name, _ := query["name"]
+		keyparam, _ := query["keys"]
+		keys := []string{}
+		if len(keyparam) > 0 {
+			keys = strings.Split(strings.Trim(keyparam[0], " \r\n\t"), ",")
+		}
+
+		var stats map[string]uint64
+		if len(name) == 0 {
+			stats = Stats()
+		} else {
+			stats = Stat(name[0])
+		}
+		stats = filterstats(stats, keys)
+		stats["timestamp"] = uint64(time.Now().UnixNano())
+
+		jsonstats, err := json.Marshal(stats)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error() + "\n"))
+			return
+		}
+
+		header := w.Header()
+		header["Content-Type"] = []string{"application/json"}
+		header["Access-Control-Allow-Origin"] = []string{"*"}
+		w.WriteHeader(200)
+		w.Write(jsonstats)
+		w.Write([]byte("\n"))
+	}
+}
+
+func filterstats(stats map[string]uint64, keys []string) map[string]uint64 {
+	if len(keys) == 0 {
+		return stats
+	}
+	m := map[string]uint64{}
+	for _, key := range keys {
+		m[key] = stats[key]
+	}
+	return m
+}
