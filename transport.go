b1e195752093229a078cf8480b8ff55f5e57581d
prataprc <prataprc@gmail.com>
2016-10-11 18:37:05 +0530

    moved package documentation to separate file (doc.go)

--- transport.go
+++ transport.go
@@ -1,27 +1,3 @@
-// Package gofast implements a high performance symmetric protocol for on the
-// wire data transport.
-//
-// opaque-space, is range of uint64 values reserved for tagging packets. They
-// shall be supplied via settings while instantiating the transport.
-//
-// messages, are golang objects implementing the Message{} interface. Message
-// objects need to be subscribed with transport before they are exchanged over
-// the transport. It is also expected that distributed systems must
-// pre-define messages and their Ids.
-//
-// message ids, need to be unique for every type of message transfered over
-// using gofast protocol, following id range is reserved for internal use:
-//
-//		0x00 - 0x0F -- reserved messages ids.
-//
-// transport instantiation steps:
-//
-//		t := NewTransport(conn, &ver, nil, settings)
-//		t.SubscribeMessage(&msg1, handler1) // subscribe message
-//		t.SubscribeMessage(&msg2, handler2) // subscribe another message
-//		t.Handshake()
-//		t.FlushPeriod(tm)                   // optional
-//		t.SendHeartbeat(tm)                 // optional
 package gofast
 
 import "sync"
