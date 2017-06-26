4fd77df13b4218e401ece791820aa491230764d6
prataprc <prataprc@gmail.com>
2017-06-26 16:09:29 +0530

    Refactor interfaces from msg.go to api.go.

---
+++ api.go
@@ -0,0 +1,60 @@
+package gofast
+
+// Streamer interface to send/receive bidirectional streams of several
+// messages.
+type Streamer interface {
+	// Response to a request, to batch the response pass flush as false.
+	Response(msg Message, flush bool) error
+
+	// Stream a single message, to batch the message pass flush as false.
+	Stream(msg Message, flush bool) error
+
+	// Close this stream.
+	Close() error
+}
+
+// BinMessage is a tuple of {id, encodedmsg-slice}
+type BinMessage struct {
+	ID   uint64
+	Data []byte
+}
+
+// Message interface, implemented by all messages exchanged via
+// gofast-transport.
+type Message interface {
+	// ID return a unique message identifier.
+	ID() uint64
+
+	// Encode message to binary blob.
+	Encode(out []byte) []byte
+
+	// Decode this message from a binary blob.
+	Decode(in []byte) (n int64)
+
+	// Size return memory foot-print of encoded message
+	Size() int64
+
+	// String representation of this message, used for logging.
+	String() string
+}
+
+// Version interface to exchange and manage transport's version.
+type Version interface {
+	// Less than supplied version.
+	Less(ver Version) bool
+
+	// Equal to the supplied version.
+	Equal(ver Version) bool
+
+	// String representation of version, for logging.
+	String() string
+
+	// Encode version into array of bytes.
+	Encode(out []byte) []byte
+
+	// Decode array of bytes to version object.
+	Decode(in []byte) (n int64)
+
+	// Size return memory foot-print encoded version
+	Size() int64
+}
--- msg.go
+++ msg.go
@@ -6,44 +6,6 @@ import "reflect"
 
 import "github.com/prataprc/golog"
 
-// Streamer interface to send/receive bidirectional streams of several
-// messages.
-type Streamer interface {
-	// Response to a request, to batch the response pass flush as false.
-	Response(msg Message, flush bool) error
-
-	// Stream a single message, to batch the message pass flush as false.
-	Stream(msg Message, flush bool) error
-
-	// Close this stream.
-	Close() error
-}
-
-// BinMessage is a tuple of {id, encodedmsg-slice}
-type BinMessage struct {
-	ID   uint64
-	Data []byte
-}
-
-// Message interface, implemented by all messages exchanged via
-// gofast-transport.
-type Message interface {
-	// ID return a unique message identifier.
-	ID() uint64
-
-	// Encode message to binary blob.
-	Encode(out []byte) []byte
-
-	// Decode this message from a binary blob.
-	Decode(in []byte) (n int64)
-
-	// Size return memory foot-print of encoded message
-	Size() int64
-
-	// String representation of this message, used for logging.
-	String() string
-}
-
 const (
 	msgStart     uint64 = 0x1000 // reserve start.
 	msgPing             = 0x1001 // to ping/echo with peer.
@@ -51,27 +13,6 @@ const (
 	msgHeartbeat        = 0x1003 // to send/receive heartbeat.
 	msgEnd              = 0x100f // reserve end.
 )
-
-// Version interface to exchange and manage transport's version.
-type Version interface {
-	// Less than supplied version.
-	Less(ver Version) bool
-
-	// Equal to the supplied version.
-	Equal(ver Version) bool
-
-	// String representation of version, for logging.
-	String() string
-
-	// Encode version into array of bytes.
-	Encode(out []byte) []byte
-
-	// Decode array of bytes to version object.
-	Decode(in []byte) (n int64)
-
-	// Size return memory foot-print encoded version
-	Size() int64
-}
 
 // handler for whoamiMsg, pingMsg, heartbeatMsg messages.
 func (t *Transport) msghandler(stream *Stream, msg BinMessage) StreamCallback {
